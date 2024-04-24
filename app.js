4f1da5f6b92888558a42d9c3dd7a31596c3969d6
Santiago <santiagobinario@gmail.com>
2024-04-23 19:34:14 -0600

    update

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const config = require('./src/config/config');
 
 const app = express();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
-
-// Rutas
 app.use('/api/auth', require('./src/routes/authRoutes'));
 
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Conectado a MongoDB'))
-.catch(error => console.error('Error de conexión a MongoDB:', error));
+.then(() => {
+    console.log('Conectado a MongoDB');
+})
+.catch((error) => {
+    console.error('Error de conexión a MongoDB:', error);
+    process.exit(1);
+});
 
-const PORT = process.env.PORT || 27017;
+const PORT = config.PORT;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
